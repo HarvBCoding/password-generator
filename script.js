12dd60a09505e0df7fd0c974a26d681c2e0ee69a
Breania Harvey <harvey.breania@gmail.com>
2021-08-31 20:26:28 -0500

    while loop bug fixed

--- script.js
+++ script.js
@@ -9,6 +9,24 @@ var upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', '
 var numericChars = ['0', '1', '2', '4', '5', '6', '7', '8', '9'];
 var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
 
+// ask user preferences with confirm
+var upperCaseConfirm = window.confirm("Would you like upper case letters?");
+var lowerCaseConfirm = window.confirm("Would you like lower case letters?");
+var numericConfirm = window.confirm("Would you like numbers?");
+var specialCharacters = window.confirm("Would you like to add special characters?");
+// ask user to confirm character length
+var passwordLength = window.prompt("How long would you like your password to be? Pick a number between 8 and 128.");
+
+// if the user enters a number more than 128, less than 8 or nothing at all an alert will tell them to pick a valid number
+while (passwordLength > 128 || passwordLength < 8 || passwordLength === null) {
+window.alert("Please enter a valid character number.");
+// passwordLength is reset to a valid character
+passwordLength = window.prompt("How long would you like your password to be? Pick a number between 8 and 128.");
+generatePassword();
+}
+
+// take the input from passwordLength and turn it into an integer
+passwordLength = parseInt(passwordLength);
 
 // functions for random character out of each array
 function getRandom(max) {
@@ -22,30 +40,11 @@ function shuffle(array) {
 
 // function to generate password
 function generatePassword() {
-  
-  // ask user to confirm character length
-  var passwordLength = window.prompt("How long would you like your password to be? Pick a number between 8 and 128.");
-  
-  // if the user enters a number more than 128, less than 8 or nothing at all an alert will tell them to pick a valid number
-  while (passwordLength > 128 || passwordLength < 8 || passwordLength === null) {
-    window.alert("Please enter a valid character number.");
-    // the function will start over to allow user to enter a valid option
-    generatePassword();
-  }
-  
-  // take the input from passwordLength and turn it into an integer
-  passwordLength = parseInt(passwordLength);
-  
-  // ask user preferences with confirm
-  var upperCaseConfirm = window.confirm("Would you like upper case letters?");
-  var lowerCaseConfirm = window.confirm("Would you like lower case letters?");
-  var numericConfirm = window.confirm("Would you like numbers?");
-  var specialCharacters = window.confirm("Would you like to add special characters?");
-  
+
   // empty array to hold characters
   var endPassword = [];
   
-// while the length of endPassword is less than passwordLength
+  // while the length of endPassword is less than passwordLength
   while (endPassword.length < passwordLength) {
     // if upperCaseConfirm is true and the length of endPassword is less than passwordLength
     if (upperCaseConfirm && endPassword.length < passwordLength) {
